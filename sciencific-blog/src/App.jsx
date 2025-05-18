ee56a7efa1f59570845bdd8c249c7443cc493348
vozmitelvlas <vozmitel.vlas228@mail.ru>
2025-05-18 12:56:19 +0300

    set project and db

---
+++ sciencific-blog/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import styled from "styled-components"
+
+const Div = styled.div`
+  text-align: center;
+`
+
+function App() {
+    return (
+        <div>
+            <Div> <i className="fa fa-calendar"></i></Div>
+        </div>
+    )
+}
+
+export default App
